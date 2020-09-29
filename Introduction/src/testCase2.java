65bed84f53b0c27a90af081e2fee204f0d6c61f4
MichnevicDiana <michnevic.diana@gmail.com>
2020-09-29 11:25:32 +0300

    Test commit

--- Introduction/src/testCase2.java
+++ Introduction/src/testCase2.java
@@ -8,11 +8,13 @@ public class testCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// test
 		
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\diana.michnevic\\Desktop\\JAVA\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "C:\\Users\\diana.michnevic\\Desktop\\JAVA\\chromedriver_win32\\chromedriver.exe"
+		 * ); WebDriver driver = new ChromeDriver();
+		 */
 		/////////////////////////
 		/*
 		 * driver.get("http://google.com"); System.out.println(driver.getTitle());
